4f099dfb7e7126281f71253d6975e54f0e73c55a
Ytratsiuk <ytratsiuk@gmail.com>
2023-02-22 10:57:35 -0700

    task1

---
+++ src/class01/HW.java
@@ -0,0 +1,21 @@
+package class01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HW {
+    public static void main(String[] args) {
+        //launch the browser
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        //navigate to amazon web site
+        driver.get("https://www.amazon.com/");
+        //print out the title and the url in the console
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        String title = driver.getTitle();
+        System.out.println(title);
+        //close the browser
+        driver.close();
+    }
+}
