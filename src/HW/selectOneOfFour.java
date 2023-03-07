9e6ac6259ce6d2dfd13cec598b0788dc16893944
Ytratsiuk <ytratsiuk@gmail.com>
2023-03-07 15:13:20 -0700

    selected

--- src/HW/selectOneOfFour.java
+++ src/HW/selectOneOfFour.java
@@ -2,7 +2,10 @@ package HW;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 //go to http://practice.syntaxtechs.net/basic-checkbox-demo.php
 //1. u need to write down the code that can select  1 check box out of 4 mentioned,
@@ -13,8 +16,13 @@ public class selectOneOfFour {
         WebDriver driver = new ChromeDriver();
         driver.get("http://practice.syntaxtechs.net/basic-checkbox-demo.php");
         driver.manage().window().maximize();
-        driver.findElement(By.xpath("//input[@value='Option-2']")).click();
-
+        List<WebElement> btns = driver.findElements(By.xpath("//input[@class='cb1-element']"));
+        for (WebElement btn : btns) {
+            if (btn.isDisplayed() && btn.isEnabled() && !btn.isSelected()) {
+                if (btn.getAttribute("value").equalsIgnoreCase("Option-2")) {
+                    btn.click();
+                }
+            }
+        }
     }
-
 }
