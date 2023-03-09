ca24860219d42b849028cc9f221f073281dec666
Ytratsiuk <ytratsiuk@gmail.com>
2023-03-08 20:54:10 -0700

    Explicit wait

--- src/HW/HiddenElement.java
+++ src/HW/HiddenElement.java
@@ -1,12 +1,15 @@
 package HW;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 //1.Open the Chrome browser and navigate to https://the-internet.herokuapp.com/dynamic_loading/1
 //2.Click on the "Start" button to initiate the loading of a hidden element
-//3.Without using Thread.sleep(), write a code that waits for the hidden element to appear using Implicit Wait
-//4.Click the "Finish" button to reveal the hidden element
+//3.write a code that waits for the hidden element to appear using Explicit Wait
 //5.Verify that the text "Hello World!" is now displayed on the page
 public class HiddenElement {
     public static void main(String[] args) {
@@ -14,6 +17,12 @@ public class HiddenElement {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
-
+        driver.findElement(By.xpath("//button")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        WebElement text = driver.findElement(By.xpath("//h4[text()='Hello World!']"));
+        wait.until(ExpectedConditions.visibilityOf(text));
+        if (text.getText().equalsIgnoreCase("hello world!")) {
+            System.out.println("the text Hello World! is now displayed on the page");
+        }
     }
 }
